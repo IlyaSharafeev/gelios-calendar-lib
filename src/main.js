07640ffabb6c4169f7c1d76621aa86d5cb7bb3cb
ilyasharafeev <ilyasharafeev01@gmail.com>
2025-08-04 19:02:08 +0000

    connect teacher schedule

--- src/main.js
+++ src/main.js
@@ -6,13 +6,17 @@ import { createVfm } from 'vue-final-modal'
 import App from './App.vue'
 import i18n from '/i18n'
 import router from './router/index.js'
+import { createPinia } from 'pinia'
 const vfm = createVfm()
 
 
 const app = createApp(App)
 
+const pinia = createPinia()
+
 app.use(router)
 app.use(vfm)
 app.use(i18n)
+app.use(pinia)
 
 app.mount('#app')
